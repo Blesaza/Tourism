34350f2a41af5c44c139316637d05ecc500e688c
Dijane_Azemi <dijane.azemi@student.uni-pr.edu>
2022-01-14 12:49:17 +0100

    T

--- Tourism.js
+++ Tourism.js
@@ -10,3 +10,18 @@ document.getElementById('button').addEventListener('click', function(){
 document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.bg-video').style.display = 'none';
 });
+
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event){
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event){
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
